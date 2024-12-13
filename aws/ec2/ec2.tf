263cbcbdee8a58f1e104325323d93bc8f21b2a3e
Monty Oh <ohhanjin0330@gmail.com>
2024-12-13 18:02:19 +0900

    [FIX] EC2 인스턴스 수정  - 주석 수정

--- aws/ec2/ec2.tf
+++ aws/ec2/ec2.tf
@@ -5,11 +5,13 @@ module "security_group" {
 }
 
 # 테스트용 임시 ec2 인스턴스
+## Amazon Linux 2023 AMI
 module "temp_ec2_instance" {
-  source = "./instance"
+  source        = "./instance"
   instance_name = "temp_ec2_instance"
   instance_type = "t2.micro"
-  ami           = "ami-0f1e61a80c7ab943e"   //  Amazon Linux 2013
+  ami           = "ami-0f1e61a80c7ab943e"
+
   vpc_security_group_ids = [module.security_group.ec2_security_group_ssh_info.id]
 
   depends_on = [module.security_group]
