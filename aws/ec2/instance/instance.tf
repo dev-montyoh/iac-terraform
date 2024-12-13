98d0d3d0b3072a04fddab52951b291b8c145b432
Monty Oh <ohhanjin0330@gmail.com>
2024-12-13 17:53:17 +0900

    [FIX] EC2 인스턴스 수정  - 인스턴스 이미지 수정  - variable 추가

--- aws/ec2/instance/instance.tf
+++ aws/ec2/instance/instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "ec2_instance" {
-  instance_type = "t2.micro"
-  ami           = "ami-0f1e61a80c7ab943e"
+  instance_type = var.instance_type
+  ami           = var.ami
   vpc_security_group_ids = var.vpc_security_group_ids
 
   tags = {
