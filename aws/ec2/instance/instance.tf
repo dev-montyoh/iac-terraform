6e74b55d76eb9d29358c5e1dcb77170e70c5cc8b
Monty Oh <ohhanjin0330@gmail.com>
2024-12-20 10:34:08 +0900

    [FIX] EC2 인스턴스 수정  - userdata 추가

--- aws/ec2/instance/instance.tf
+++ aws/ec2/instance/instance.tf
@@ -1,7 +1,8 @@
 resource "aws_instance" "ec2_instance" {
-  instance_type = var.instance_type
-  ami           = var.ami
+  instance_type          = var.instance_type
+  ami                    = var.ami
   vpc_security_group_ids = var.vpc_security_group_ids
+  user_data              = var.user_data
 
   tags = {
     Name = var.instance_name
