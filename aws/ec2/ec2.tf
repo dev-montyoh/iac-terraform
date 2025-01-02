ccccf90e93ff1d7c6bdca7f8b252fe51915e61bd
Monty Oh <ohhanjin0330@gmail.com>
2025-01-02 19:51:23 +0900

    [FIX] EC2 수정  - 임시 인스턴스 삭제  - database server 인스턴스 추가  - userdata 추가

--- aws/ec2/ec2.tf
+++ aws/ec2/ec2.tf
@@ -4,20 +4,6 @@ module "security_group" {
   VPC_ID = var.VPC_ID
 }
 
-# 테스트용 임시 ec2 인스턴스
-## Amazon Linux 2023 AMI
-module "temp_ec2_instance" {
-  source        = "./instance"
-  instance_name = "temp_ec2_instance"
-  instance_type = "t2.micro"
-  ami           = "ami-0f1e61a80c7ab943e"
-  user_data = file("scripts/temp_ec2_userdata.sh")
-
-  vpc_security_group_ids = [module.security_group.ec2_security_group_ssh_info.id]
-
-  depends_on = [module.security_group]
-}
-
 #   DB 서버 전용 인스턴스
 ##  Amazon Linux 2023 AMI
 module "ec2_db_server_instance" {
--- scripts/ec2_database_server_userdata.sh
+++ scripts/ec2_database_server_userdata.sh
@@ -7,8 +7,6 @@ yum install https://dev.mysql.com/get/mysql80-community-release-el9-5.noarch.rpm
 # install mysql-server@8.0
 yum install mysql-community-server -y
 
+# mysql 실행 설정
 systemctl start mysqld
 systemctl enable --now mysqld
-
-# 임시 비밀번호 확인
-grep 'temporary password' /var/log/mysqld.log
