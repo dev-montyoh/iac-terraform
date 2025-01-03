56d6568b1c424f99ea9f449586e031cc28f3816e
오한진 <monty@plgrim.com>
2025-01-03 10:35:23 +0900

    [FIX] AWS EC2 userdata 수정  - yum -> dnf 변경  - redis 설정 추가

--- scripts/ec2_database_server_userdata.sh
+++ scripts/ec2_database_server_userdata.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-yum update -y
+dnf update -y
 
-# install mysql yum repository
-yum install https://dev.mysql.com/get/mysql80-community-release-el9-5.noarch.rpm -y
+# MySQL 설치
+## install mysql yum repository
+dnf install https://dev.mysql.com/get/mysql80-community-release-el9-5.noarch.rpm -y
 
-# install mysql-server@8.0
-yum install mysql-community-server -y
+## install mysql-server@8.0
+dnf install mysql-community-server -y
 
-# mysql 실행 설정
+## mysql 실행 설정
 systemctl start mysqld
 systemctl enable --now mysqld
+
+# Redis 설치
+## install redis6
+dnf install redis6 -y
+
+## redis 실행 설정
+systemctl start redis6
+systemctl enable --now redis6
