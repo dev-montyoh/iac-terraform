982466baa6e33a6b85f6e9a32ade99b3107609ca
Monty Oh <ohhanjin0330@gmail.com>
2025-01-15 09:25:50 +0900

    [NEW] AWS UserRole 모듈 추가  - 테스트용 UserRole 생성

--- aws/iam/iam.tf
+++ aws/iam/iam.tf
@@ -14,3 +14,17 @@ module "user" {
   email    = each.value.email
   groups   = each.value.groups
 }
+
+#  USER_ROLE
+##  EC2 인스턴스 기동 시간 설정을 위한 USER_ROLE 생성
+module "user_role_ec2_ssm" {
+  source = "./role"
+
+  name = ""
+  path = ""
+}
+
+resource "aws_iam_role_policy_attachment" "attach_ec2_full_access" {
+  role   = module.user_role_ec2_ssm.iam_role_info.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
---
+++ aws/iam/role/role.tf
@@ -0,0 +1,12 @@
+resource "aws_iam_role" "this" {
+  name = var.name
+  path = var.path
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect    = "Allow"
+      Principal = { Service = "ssm.amazonaws.com" }
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
