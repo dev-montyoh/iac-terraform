63da30e29dc8fcedb0dc9e1f91a8f4b4ccef21eb
Monty Oh <ohhanjin0330@gmail.com>
2025-01-16 18:25:32 +0900

    [NEW] AWS SystemsManager 추가  - 시간 수정

--- aws/ssm/maintenance_window/task/task.tf
+++ aws/ssm/maintenance_window/task/task.tf
@@ -1,45 +1,45 @@
 ## 테스트 임시 추가
-resource "aws_iam_policy" "ssm_maintenance_policy" {
-  name        = "SSMMaintenancePolicy"
-  description = "Policy for SSM Maintenance Window Tasks"
-
-  policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "ssm:SendCommand",
-          "ec2:StartInstances",
-          "ec2:StopInstances",
-          "ssm:StartAutomationExecution"  # 추가
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role" "ssm_maintenance_role" {
-  name               = "SSMMaintenanceRole"
-  assume_role_policy = jsonencode({
-    Version   = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = {
-          Service = "ssm.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_maintenance_policy_attachment" {
-  role       = aws_iam_role.ssm_maintenance_role.name
-  policy_arn = aws_iam_policy.ssm_maintenance_policy.arn
-}
+# resource "aws_iam_policy" "ssm_maintenance_policy" {
+#   name        = "SSMMaintenancePolicy"
+#   description = "Policy for SSM Maintenance Window Tasks"
+#
+#   policy = jsonencode({
+#     Version   = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect = "Allow"
+#         Action = [
+#           "ssm:SendCommand",
+#           "ec2:StartInstances",
+#           "ec2:StopInstances",
+#           "ssm:StartAutomationExecution"  # 추가
+#         ]
+#         Resource = "*"
+#       }
+#     ]
+#   })
+# }
+#
+# resource "aws_iam_role" "ssm_maintenance_role" {
+#   name               = "SSMMaintenanceRole"
+#   assume_role_policy = jsonencode({
+#     Version   = "2012-10-17"
+#     Statement = [
+#       {
+#         Effect    = "Allow"
+#         Principal = {
+#           Service = "ssm.amazonaws.com"
+#         }
+#         Action = "sts:AssumeRole"
+#       }
+#     ]
+#   })
+# }
+#
+# resource "aws_iam_role_policy_attachment" "ssm_maintenance_policy_attachment" {
+#   role       = aws_iam_role.ssm_maintenance_role.name
+#   policy_arn = aws_iam_policy.ssm_maintenance_policy.arn
+# }
 
 resource "aws_ssm_maintenance_window_task" "start_task" {
   window_id = var.ssm_maintenance_window_start_id
@@ -54,9 +54,6 @@ resource "aws_ssm_maintenance_window_task" "start_task" {
   priority        = 1
   max_concurrency = "1"
   max_errors      = "1"
-
-  # IAM Role 추가
-#  service_role_arn = aws_iam_role.ssm_maintenance_role.arn
 
   task_invocation_parameters {
     automation_parameters {
@@ -83,9 +80,6 @@ resource "aws_ssm_maintenance_window_task" "stop_task" {
   priority        = 1
   max_concurrency = "1"
   max_errors      = "1"
-
-  # IAM Role 추가
-#  service_role_arn = aws_iam_role.ssm_maintenance_role.arn
 
   task_invocation_parameters {
     automation_parameters {
