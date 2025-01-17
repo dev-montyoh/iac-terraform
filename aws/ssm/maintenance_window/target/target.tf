f363620848f3d256180bfcca92a822e8f40287ef
오한진 <monty@plgrim.com>
2025-01-17 09:30:57 +0900

    [FIX] AWS EC2 userdata 수정  - 테스트 중

--- aws/ssm/maintenance_window/target/target.tf
+++ aws/ssm/maintenance_window/target/target.tf
@@ -1,20 +1,20 @@
 # 유지 관리 기간/타겟 설정
 resource "aws_ssm_maintenance_window_target" "start_target" {
   window_id     = var.ssm_maintenance_window_start_id
-  name          = "StartEC2Target"
+  name          = "SSMMaintenanceWindowTargetStartEC2"
   resource_type = "INSTANCE"
   targets {
-    key = "InstanceIds"
+    key    = "InstanceIds"
     values = [var.ec2_instance_database_server_id]
   }
 }
 
 resource "aws_ssm_maintenance_window_target" "stop_target" {
   window_id     = var.ssm_maintenance_window_stop_id
-  name          = "StopEC2Target"
+  name          = "SSMMaintenanceWindowTargetStopEC2"
   resource_type = "INSTANCE"
   targets {
-    key = "InstanceIds"
+    key    = "InstanceIds"
     values = [var.ec2_instance_database_server_id]
   }
 }
