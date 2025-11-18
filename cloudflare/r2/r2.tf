2e2328901cdf9d8926c951e01529cfca9e6c8ed1
Monty Oh <ohhanjin0330@gmail.com>
2025-11-18 16:03:44 +0900

    [NEW] Cloudflare - Cloudflare R2 추가

---
+++ cloudflare/r2/r2.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source = "cloudflare/cloudflare"
+    }
+  }
+}
+
+# R2 버킷 생성
+resource "cloudflare_r2_bucket" "content" {
+  account_id = var.CLOUDFLARE_ACCOUNT_ID
+  name       = var.bucket_name
+  location   = "APAC"
+}
