3fa5eb7481f631b0f6a8ed9ee18d52fd7ff377de
Monty Oh <ohhanjin0330@gmail.com>
2025-12-02 09:25:56 +0900

    [FIX] DNS_RECORD - 오류로 인한 임시 롤백

--- cloudflare/cloudflare.tf
+++ cloudflare/cloudflare.tf
@@ -52,15 +52,15 @@ resource "cloudflare_dns_record" "dev_monty_web_payment" {
 }
 
 ##  서브 도메인 - R2 버킷 접근 도메인
-resource "cloudflare_dns_record" "dev_monty_cdn" {
-  zone_id = var.CLOUDFLARE_ZONE_ID
-  name    = "cdn.dev-monty.me"
-  type    = "CNAME"
-  comment = "cdn.dev-monty.me record"
-  content = "r2.cloudflarestorage.com"
-  ttl     = 1
-  proxied = true
-}
+# resource "cloudflare_dns_record" "dev_monty_cdn" {
+#   zone_id = var.CLOUDFLARE_ZONE_ID
+#   name    = "cdn.dev-monty.me"
+#   type    = "CNAME"
+#   comment = "cdn.dev-monty.me record"
+#   content = "r2.cloudflarestorage.com"
+#   ttl     = 1
+#   proxied = true
+# }
 
 ##  R2 버킷 생성
 ### Content 
