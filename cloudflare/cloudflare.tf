36fbbd872a5d3d89a3fbaab05910b55525241367
Monty <ohhanjin0330@gmail.com>
2026-06-09 01:05:31 +0000

    feat: R2 common-static 버킷 및 static.montyoh.dev 커스텀 도메인 연결

--- cloudflare/cloudflare.tf
+++ cloudflare/cloudflare.tf
@@ -6,6 +6,15 @@ terraform {
   }
 }
 
+# R2 버킷 - 공통 정적 파일 (static.montyoh.dev)
+module "r2" {
+  source                = "./r2"
+  CLOUDFLARE_ACCOUNT_ID = var.CLOUDFLARE_ACCOUNT_ID
+  bucket_name           = "common-static"
+  domain                = "static.montyoh.dev"
+  zone_id               = var.CLOUDFLARE_ZONE_ID_MONTYOH_DEV
+}
+
 # montyoh.dev 도메인 설정
 ##  루트 도메인 - 웹 접속
 resource "cloudflare_dns_record" "montyoh_dev_root" {
--- cloudflare/r2/variable.tf
+++ cloudflare/r2/variable.tf
@@ -1,2 +1,4 @@
 variable "CLOUDFLARE_ACCOUNT_ID" { type = string }
 variable "bucket_name" { type = string }
+variable "domain" { type = string }
+variable "zone_id" { type = string }
